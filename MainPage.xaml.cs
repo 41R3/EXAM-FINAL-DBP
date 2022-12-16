dcd55f74c102c7d773c45cd9141012aef38a5200
S-Carlin <75504946+41R3@users.noreply.github.com>
2022-12-16 00:26:10 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FORMCV
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void ButtonSubmitClicked(System.Object sender, System.EventArgs e)
+        {
+            
+            var input_name = name.Text;
+            var input_age = age.Text;
+            var input_adress = adress.Text;
+            var input_contact = contact.Text;
+            var input_education = education.Text;
+            var input_ocupation = ocupation.Text;
+            
+
+            Application.Current.MainPage.Navigation.PushModalAsync(new dinamicpage(input_name, input_age, input_adress,
+                input_contact, input_education,input_ocupation), true);
+
+            
+        }
+        void ButtonPlusOcupationClicked(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your Occupation" };
+            ocup2.Children.Add(entry);
+            
+        }
+        void ButtonPlusEducationClicked(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your Occupation" };
+            edu.Children.Add(entry);
+            
+        }
+    }
+}
---
+++ dinamicpage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace FORMCV
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class dinamicpage : ContentPage
+    {
+        private string mainContentProperty1;
+        public string MainContentProperty1
+        {
+            get { return mainContentProperty1; }
+            set { mainContentProperty1 = value;
+                OnPropertyChanged(nameof(MainContentProperty1));
+            }
+
+        }
+        public string MainContentProperty2
+        {
+            get { return mainContentProperty1; }
+            set
+            {
+                mainContentProperty1 = value;
+                OnPropertyChanged(nameof(MainContentProperty2));
+            }
+
+        }
+        public string MainContentProperty3
+        {
+            get { return mainContentProperty1; }
+            set
+            {
+                mainContentProperty1 = value;
+                OnPropertyChanged(nameof(MainContentProperty3));
+            }
+
+        }
+    
+        public string MainContentProperty4
+        {
+            get { return mainContentProperty1; }
+            set
+            {
+                mainContentProperty1 = value;
+                OnPropertyChanged(nameof(MainContentProperty4));
+            }
+
+        }
+        
+        public string MainContentProperty5
+        {
+            get { return mainContentProperty1; }
+            set
+            {
+                mainContentProperty1 = value;
+                OnPropertyChanged(nameof(MainContentProperty5));
+            }
+
+        }
+        
+        public string MainContentProperty6
+        {
+            get { return mainContentProperty1; }
+            set
+            {
+                mainContentProperty1 = value;
+                OnPropertyChanged(nameof(MainContentProperty6));
+            }
+
+        }
+        
+
+        public dinamicpage(string mainContent1, string mainContent2, string mainContent3,string mainContent4, string mainContent5, string mainContent6 )
+        {
+            InitializeComponent();
+            BindingContext = this;
+            MainContentProperty1 = mainContent1;
+            MainContentProperty2 = mainContent2;
+            MainContentProperty3 = mainContent3;
+            MainContentProperty4 = mainContent4;
+            MainContentProperty5 = mainContent5;
+            MainContentProperty6 = mainContent6;
+            
+        }
+        
+        
+        
+    }
+}
